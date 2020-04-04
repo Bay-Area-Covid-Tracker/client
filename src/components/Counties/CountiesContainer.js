7187152cebcb95df58b80cbd9cc4bedb820367dd
Christian Lapinig <clapinig93@gmail.com>
2020-04-03 20:37:59 -0700

    Add base counties cards in CountiesContainer

--- src/components/Counties/CountiesContainer.js
+++ src/components/Counties/CountiesContainer.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import Grid from '@material-ui/core/Grid';
 
 import CountyCard from './CountyCard';
 
@@ -7,7 +8,49 @@ export default function CountiesContainer({ dailyReport }) {
 	return (
 		<div>
 			<p>Counties container</p>
-			<CountyCard county={dailyReport.Alameda} />
+			<Grid
+				container
+				direction="row"
+				justify="center"
+				alignItems="center"
+				spacing={3}
+			>
+				<Grid item>
+					<CountyCard county={dailyReport['Alameda']} />
+				</Grid>
+				<Grid item>
+					<CountyCard county={dailyReport['Contra Costa']} />
+				</Grid>
+				<Grid item>
+					<CountyCard county={dailyReport['Marin']} />
+				</Grid>
+				<Grid item>
+					<CountyCard county={dailyReport['Napa']} />
+				</Grid>
+				<Grid item>
+					<CountyCard county={dailyReport['San Mateo']} />
+				</Grid>
+			</Grid>
+			<Grid
+				container
+				direction="row"
+				justify="center"
+				alignItems="center"
+				spacing={3}
+			>
+				<Grid item>
+					<CountyCard county={dailyReport['San Francisco']} />
+				</Grid>
+				<Grid item>
+					<CountyCard county={dailyReport['Santa Clara']} />
+				</Grid>
+				<Grid item>
+					<CountyCard county={dailyReport['Solano']} />
+				</Grid>
+				<Grid item>
+					<CountyCard county={dailyReport['Sonoma']} />
+				</Grid>
+			</Grid>
 		</div>
 	);
 }
--- src/components/Counties/__tests__/CountyCard.test.js
+++ src/components/Counties/__tests__/CountyCard.test.js
@@ -1,13 +1,32 @@
 import React from 'react';
-import { shallow } from 'enzyme';
+import { shallow, mount } from 'enzyme';
 
 import CountyCard from '../CountyCard';
+
+const county = {
+	"id": 6055,
+	"county": "Napa",
+	"state": "California",
+	"country": "US",
+	"lastUpdate": "2020-03-30 22:52:45",
+	"lat": 38.5074,
+	"lon": -122.333,
+	"confirmed": 14,
+	"deaths": 0
+};
 
 describe('tests for CountyCard', () => {
 	describe('snapshot tests', () => {
 		it('renders without crashing', () => {
 			const tree = shallow(<CountyCard />);
 			expect(tree.html()).toMatchSnapshot();
+		});
+	});
+
+	describe('unit tests', () => {
+		it('renders with passed county prop', () => {
+			const wrapper = mount(<CountyCard county={county} />);
+			expect(wrapper.props().county).toEqual(county);
 		});
 	});
 });
